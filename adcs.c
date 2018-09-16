c3a9398444b56685ddee526e1e681530866f8fe7
Antti Alhonen <antti.alhonen@iki.fi>
2018-09-16 18:25:17 +0300

    adc data structures and most of the configuration

--- adcs.c
+++ adcs.c
@@ -1,1 +1,82 @@
 #include <stdint.h>
+
+
+#define CONF_ADC_SEQ(adc_,len_,s1_,s2_,s3_,s4_,s5_,s6_,s7_,s8_,s9_,s10_,s11_,s12_,s13_,s14_,s15_,s16_) do{ \
+	adc_->SQR1 = (s4_)<<24 | (s3_)<<18 | (s2_)<<12 | (s1_)<<6 | (len_-1); \
+	adc_->SQR2 = (s9_)<<24 | (s8_)<<18 | (s7_)<<12 | (s6_)<<6 | (s5_); \
+	adc_->SQR3 = (s14_)<<24 | (s13_)<<18 | (s12_)<<12 | (s11_)<<6 | (s10_); \
+	adc_->SQR4 = (s16_)<<6 | (s15_); \
+	} while(0)
+
+
+// If differential measurements will be used, remember to add a calibration cycle for diff meas.
+static void init_calib_adc(ADC_TypeDef *adc)
+{
+	adc->CR = 0; // DEEPPWD (deep power down) mode off (reset state is bit29 high).
+	adc->CR = 1UL<<28; // Voltage regulator on
+	delay_us(100); // minimum required 10us per datasheet, but let's be sure to get the calibration values accurate.
+	adc->CR = 1UL<<28 /* Keep Vreg on*/ | 1UL<<16 /* Calibrate for nonlinearity as well */;
+	adc->CR |= 1UL<<31; // Start calibration
+	delay_us(1);
+	while(adc->CR & (1UL<<31)) ; // Wait for calibration success
+	adc->ISR = 1UL; // Clear ADRDY
+	adc->CR |= 1UL; // Enable ADC
+	while(adc->ISR & 1UL) ; // Wait for enable success
+	adc->CR |= 1UL<<8 /*BOOST bit for fADC>20MHz*/;
+}
+
+#define TRIG_SW      0b00UL
+#define TRIG_RISING  0b01UL
+#define TRIG_FALLING 0b10UL
+#define TRIG_BOTH    0b11UL
+
+#define RESO_16B 0b000UL
+#define RESO_14B 0b001UL
+#define RESO_12B 0b010UL
+#define RESO_10B 0b011UL
+#define RESO_8B  0b100UL
+
+void init_adcs()
+{
+
+	// Clear DEEPPWD
+	// ADVREGEN = 1
+	// Wait for TADCVREG_STUP (10 us min)
+	
+	// ADCALDIF = 0
+	// ADCALLIN = 1
+	// ADCAL = 1
+	// Wait until ADCAL goes 0
+	// ADC_ISR write 1 to ADRDY
+	// ADEN = 1
+	// Wait until ADRDY goes 1
+
+
+	init_calib_adc(ADC1);
+	CONF_ADC_SEQ(ADC1, ADC1_SEQ_LEN, ADC1_SEQ);
+	ADC1->CFGR = 1UL<<31 /*Injected queue disable*/ |
+
+		     0UL<<26 /*AWD1 chan*/ | 0UL<<23 /*Enable AWD1?*/ | 1UL<<22 /*AWD1 on the single (configured) channel only?*/ |
+
+		     1UL<<13 /*Continuous mode?*/ |
+		     TRIG_SW<<10 /*Trigger type*/ |
+		     0UL<<5 /*Trigger event number*/ |
+		     RESO_16B<<2 /*Resolution*/ |
+		     0b11UL /*11 = DMA circular, 01 = DMA one shot*/;
+
+	// Analog watchdog 2 enabled on channels (bit index = channel number):
+	ADC1->AWD2CR = 0; 
+	ADC1->LTR2 = 0;
+	ADC1->HTR2 = 65535;
+
+	ADC1->AWD3CR = 0; 
+	ADC1->LTR3 = 0;
+	ADC1->HTR3 = 65535;
+
+	init_calib_adc(ADC2);
+	CONF_ADC_SEQ(ADC2, ADC2_SEQ_LEN, ADC2_SEQ);
+
+	init_calib_adc(ADC3);
+	CONF_ADC_SEQ(ADC3, ADC3_SEQ_LEN, ADC3_SEQ);
+
+}
--- adcs.h
+++ adcs.h
@@ -22,7 +22,67 @@
 #include <stdint.h>
 
 
-typedef struct
+/*
+	All analog values are converted by ADC1, ADC2 and ADC3, all by using DMA.
+	Some analog things are timing-critical, some are not - the required timing
+	defines the order the values are converted in.
+
+	Since DMA is used, data is stored in the memory in the same order.
+
+	ADC sequence configuration, and the data structures need to be kept in sync.
+
+	Some signals are connected to multiple ADCs, which is mentioned in comments.
+
+	Note that channel numbers on separate ADCs are unique by ADC: for example,
+	ADC1 1+ is a different channel to ADC3 1+. On the other hand, ADC12 7+ is the
+	same channel 7 on both ADC1 and ADC2.
+
+	While ADC1 channels need to reside in adc1_group, and so on for ADC2 and ADC3,
+	there is some freedom in reorganizing channels shared by multiple ADCs (e.g.,
+	there are quite a few ADC12 channels, and even some ADC123.)
+
+*/
+
+typedef struct __attribute__((packed))
 {
-	
-} adc1_data_t;
+	uint16_t bms_temp_contacts;     // ADC1   16+ PA0   Charger contact temperature NTC
+	uint16_t bms_temp_plat_mosfets; // ADC1   17+ PA1   Platform power switch MOSFET&fuse temperature NTC
+	uint16_t mc0_imeasb;            // ADC1   2+  PF11  Motor controller 0 phase B current measurement
+	uint16_t mc0_imeasc;            // ADC12  7+  PA7   Motor controller 0 phase C current measurement
+} adc1_group_t;
+
+typedef struct __attribute__((packed))
+{
+	uint16_t mc1_imeasb;            // ADC12  15+ PA3   Motor controller 1 phase B current measurement
+	uint16_t mc1_imeasc;            // ADC12  14+ PA2   Motor controller 1 phase C current measurement
+	uint16_t vbat_meas;             // ADC12  18+ PA4   Battery voltage
+	uint16_t cha_vin_meas;          // ADC12  4+  PC4   Charger input voltage
+	uint16_t cha_vinbus_meas;       // ADC12  8+  PC5   Charger input voltage after the input ideal diode MOSFET
+	uint16_t cha_currmeasa;         // ADC12  9+  PB0   Charger sync buck phase A inductor current
+	uint16_t cha_currmeasb;         // ADC12  5+  PB1   Charger sync buck phase B inductor current
+} adc2_group_t;
+
+typedef struct __attribute__((packed))
+{
+	uint16_t epc_stray_estimate;    // ADC3   5+  PF3   3DTOF sensor stray light estimate measurement (phototransistor), muxed from active sensor
+	uint16_t eb_analog1;            // ADC3   6+  PF10  Extension B analog in 1
+	uint16_t eb_analog2;            // ADC123 10+ PC0   Extension B analog in 2
+	uint16_t bms_mainfet_g_meas;    // ADC3   1+  PC3   Main power switch MOSFET gate voltage
+	uint16_t bms_appfet_g_meas;     // ADC3   14+ PH3   Application power switch MOSFET gate voltage
+	uint16_t bms_vref3;             // ADC3   15+ PH4   TI BMS chip 3.0V reference voltage
+	uint16_t bms_vmeas;             // ADC3   16+ PH5   TI BMS chip cell measurement voltage
+	uint16_t bms_temp_app_mosfets;  // ADC3   13+ PH2   Application power switch MOSFET&fuse temperature NTC
+	uint16_t bms_temp_battery;      // ADC123 12+ PC2   Battery temperature NTC
+	uint16_t cpu_temp;              // ADC3   18+ internal
+} adc3_group_t;
+
+#define ADC1_SEQ_LEN 4
+#define ADC1_SEQ 16,17,2,7,0,0,0,0,0,0,0,0,0,0,0,0
+
+#define ADC2_SEQ_LEN 7
+#define ADC2_SEQ 15,14,18,4,8,9,5,0,0,0,0,0,0,0,0,0
+
+#define ADC3_SEQ_LEN 10
+#define ADC3_SEQ 5,6,10,1,14,15,16,13,12,18,0,0,0,0,0,0
+
+
